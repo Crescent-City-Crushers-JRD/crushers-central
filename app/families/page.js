2c417d0156ddf6ed4db3122310ff7a067ce03de8
Khaled Alquaddoomi <kalquaddoomi@gmail.com>
2026-09-22 15:14:57 -0500

    CVM Size Charts

--- app/families/page.js
+++ app/families/page.js
@@ -14,6 +14,8 @@ export default function ParentsPage() {
                     <p>
                         <a className={"text-blue-600 hover:underline hover:text-purple-500"} href={"https://cvmsports.com/collections/crescent-city"} target="_blank" rel="noopener noreferrer">
                             CVM Sports Crescent City Gear</a>
+                    </p>
+                    <p>
                         <a className={"text-blue-600 hover:underline hover:text-purple-500"}  href={"https://cvmsports.com/pages/size-charts-1"} target="_blank" rel="noopener noreferrer">
                             CVM Sports Size Charts
                         </a>
