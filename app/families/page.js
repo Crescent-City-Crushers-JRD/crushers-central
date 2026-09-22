c757918d75601a5b3bdfa15c6f1056cb59a5c845
Khaled Alquaddoomi <kalquaddoomi@gmail.com>
2026-09-22 17:15:24 -0500

    Layout of surveys, fixed hydration

--- app/families/page.js
+++ app/families/page.js
@@ -10,7 +10,7 @@ export default function ParentsPage() {
                 <p>Please check out important information below:</p>
 
                 <h3 className={"text-3xl font-bold mt-10 text-center"}>Crushers Jerseys and Gear Store</h3>
-                <p className={"border-b-2 pb-2 "}>
+                <div className={"border-b-2 pb-2 "}>
                     <p>
                         <a className={"text-blue-600 hover:underline hover:text-purple-500"} href={"https://cvmsports.com/collections/crescent-city"} target="_blank" rel="noopener noreferrer">
                             CVM Sports Crescent City Gear</a>
@@ -24,7 +24,21 @@ export default function ParentsPage() {
                         sponsorship completed before this shop closes. It does take time to make and delivery jerseys and gear. If you would like to order
                         now and we will work out a patch of sponsors and then refund families, please do so.</p>
                     <p className={"font-bold italic text-sm mt-3"}>Custom jersey production typically takes 5–8 weeks from the date of our store close date.</p>
-                </p>
+                </div>
+                <div className={"justify-start mt-2 pb-2 font-mono border-b-2"}>
+
+                    <p className={"mt-3"}>
+                        We are working with the Tulane School of Medicine to get all the jersey’s purchased for the team.
+                        Look through the jersey store to select the style and size that your skater wants. The store is open till Oct. 10th.
+                    </p>
+                    <p className={"mt-3"}>
+                        If you do order a jersey for your skater and we get sponsorship afterwards, we will work on a sponsor decal or patch for
+                        uniforms and will refund purchases if we can.
+                    </p>
+                    <p className={"mt-2"}> If would prefer to wait for us to finalize Sponsorship and place orders for all skaters who did not buy their own then please complete this survey: <a className={"text-blue-600 hover:underline hover:text-purple-500"} href={"https://forms.gle/4L493s7RB3kEHcdi8"} target="_blank" rel="noopener noreferrer">
+                        Jersey Size Survey
+                    </a></p>
+                </div>
                 <h3 className={"text-3xl font-bold mt-10 text-center"}>Surveys</h3>
                 <ul className={"flex flex-col justify-start mt-2 font-mono list-disc list-outside text-lg"}>
                     <li className={"mt-2 pb-2 font-mono border-b-2"}>
@@ -35,19 +49,7 @@ export default function ParentsPage() {
                         </p>
 
                     </li>
-                    <li className={"justify-start mt-2 pb-2 font-mono border-b-2"}>
 
-                        <p className={"mt-3"}>
-                            We are working with the Tulane School of Medicine to get all the jersey’s purchased for the team.
-                            Look through the jersey store to select the style and size that your skater wants. The store is open till Oct. 10th.
-                            If you do order a jersey for your skater and we get sponsorship afterwards, we will work on a sponsor decal or patch for
-                            uniforms and will refund purchases if we can.
-                        </p>
-
-                        <p className={"mt-2"}> Then complete this survey: <a className={"text-blue-600 hover:underline hover:text-purple-500"} href={"https://forms.gle/4L493s7RB3kEHcdi8"} target="_blank" rel="noopener noreferrer">
-                            Jersey Size Survey
-                        </a></p>
-                    </li>
                 </ul>
                 <h3 className={"text-3xl font-bold mt-10 text-center"}>Family Info</h3>
                 <p className={"justify-start mt-10 pb-2 font-mono border-b-2 text-xl"}>
